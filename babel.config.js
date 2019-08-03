c46a9866608c1c9dd4731f792060e61763552380
Amin Taghipour <amin.taghipour27@gmail.com>
2019-08-03 21:12:19 +0430

    two babel plugins was installed

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,10 @@ module.exports = function(api) {
     api.cache(true);
 
     const presets = ["@babel/preset-env", "@babel/preset-react"];
+    const plugins = [
+        "@babel/plugin-proposal-class-properties",
+        "@babel/plugin-transform-arrow-functions"
+    ];
 
-    return { presets };
+    return { presets, plugins };
 };
