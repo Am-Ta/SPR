99f5d5080c7db1effad6c0fa8381cdf70aa9d307
Amin Taghipour <amin.taghipour27@gmail.com>
2019-08-03 16:31:48 +0430

    the webpack-dev-server and html-webpack-plugin was installed and the scripts property for the package.json file was added and the hot module replacement was implement

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
+const webpack = require("webpack");
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.join(__dirname, "/dist"),
         filename: "bundle.js"
+    },
+    devServer: {
+        hot: true
     },
     module: {
         rules: [
@@ -13,5 +18,11 @@ module.exports = {
                 use: "babel-loader"
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
 };
