b7d5dd33531024ad737d0972b1d0b9a061ded195
Amin Taghipour <amin.taghipour27@gmail.com>
2019-08-03 16:57:42 +0430

    the react and react-dom was installed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    const appComp = (
+        <div className="app">
+            <h1>Hello World</h1>
+        </div>
+    );
+
+    return appComp;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// Hot Module Replacement
 if (module.hot) {
     module.hot.accept();
 }
